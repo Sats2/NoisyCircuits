e33ae5ff83e5365f6fa8756efbbc6764ec1b2cc1
Sats2 <sathyamhegde@gmail.com>
2026-01-06 11:30:21 +0100

    Replaced dense matrix to sparse.
    Modified:
     - BuildQubitGateModel.py to use sparse matrix representation for noise instructions.
     - ParallelExecutor.py to support parallel execution with sparse matrix operations.
     - profiling.py to log memory usage with sparse matrices and multi-core execution.
    
    New:
     - profiling_memory_with_sparse.log - memory profiling log for sparse matrix usage.
     - profiling_memory_with_sparse_multiqubit.log - memory profiling log for sparse matrix usage with multi-qubit circuits (fully updated file to be uploaded later.).
    
    Deleted:
     - __pycache__ files related to modified files.

--- examples/profiling.py
+++ examples/profiling.py
@@ -2,11 +2,12 @@ from NoisyCircuits import QuantumCircuit as QC
 from NoisyCircuits.utils.GetNoiseModel import GetNoiseModel
 import pickle
 import os
-from scipy.sparse import csr_matrix, csc_matrix
 import json
 from memory_profiler import profile
 
-memory_log = open("profiling_memory.log", "w+")
+memory_log = open("profiling_memory_with_sparse_multiqubit.log", "w")
+memory_log.write("Memory profiling log for NoisyCircuits with 10 cores parallelization\n")
+memory_log.flush()
 
 @profile(stream=memory_log)
 def get_noise_model():
@@ -31,92 +32,21 @@ def create_circuit_object(num_qubits,
          verbose=verbose)
     return nqc
 
-@profile(stream=memory_log)
-def memory_single_matrix(nqc):
-    dense_mats = {}
-    for q_num in nqc.single_qubit_instructions:
-        q_res = {}
-        for op_name in ["x", "sx"]:
-            op_list = []
-            ops = nqc.single_qubit_instructions[q_num][op_name]["kraus_operators"]
-            for op in ops:
-                op_list.append(op)
-            q_res[op_name] = op_list
-        dense_mats[q_num] = q_res
-    return dense_mats    
-
-@profile(stream=memory_log)
-def memory_csr_matrix(nqc):
-    sparse_mats = {}
-    for q_num in nqc.single_qubit_instructions:
-        q_res = {}
-        for op_name in ["x", "sx"]:
-            op_list = []
-            ops = nqc.single_qubit_instructions[q_num][op_name]["kraus_operators"]
-            for op in ops:
-                op_list.append(csr_matrix(op))
-            q_res[op_name] = op_list
-        sparse_mats[q_num] = q_res
-    return sparse_mats
-
-@profile(stream=memory_log)
-def memory_csc_matrix(nqc):
-    sparse_mats = {}
-    for q_num in nqc.single_qubit_instructions:
-        q_res = {}
-        for op_name in ["x", "sx"]:
-            op_list = []
-            ops = nqc.single_qubit_instructions[q_num][op_name]["kraus_operators"]
-            for op in ops:
-                op_list.append(csc_matrix(op))
-            q_res[op_name] = op_list
-        sparse_mats[q_num] = q_res
-    return sparse_mats
-
-@profile(stream=memory_log)
-def dense_cz_mats(nqc):
-    dense_mats = {}
-    for key_pair in nqc.two_qubit_instructions["cz"].keys():
-        ops = nqc.two_qubit_instructions["cz"][key_pair]["operators"]
-        dense_mats[key_pair] = ops
-    return dense_mats
-
-@profile(stream=memory_log)
-def csr_cz_mats(nqc):
-    sparse_mats = {}
-    for key_pair in nqc.two_qubit_instructions["cz"].keys():
-        ops = nqc.two_qubit_instructions["cz"][key_pair]["operators"]
-        op_list = []
-        for op in ops:
-            op_list.append(csr_matrix(op))
-        sparse_mats[key_pair] = op_list
-    return sparse_mats
-
-@profile(stream=memory_log)
-def csc_cz_mats(nqc):
-    sparse_mats = {}
-    for key_pair in nqc.two_qubit_instructions["cz"].keys():
-        ops = nqc.two_qubit_instructions["cz"][key_pair]["operators"]
-        op_list = []
-        for op in ops:
-            op_list.append(csc_matrix(op))
-        sparse_mats[key_pair] = op_list
-    return sparse_mats
 
 if __name__ == "__main__":
     noise_model = get_noise_model()
-    nqc = create_circuit_object(num_qubits=10,
-                                noise_model=noise_model,
-                                num_cores=4,
-                                num_trajectories=1000,
-                                threshold=1e-8,
-                                jsonize=False,
-                                verbose=False)
-    single_qubit_dense = memory_single_matrix(nqc)
-    single_qubit_csr = memory_csr_matrix(nqc)
-    single_qubit_csc = memory_csc_matrix(nqc)
-    two_qubit_dense = dense_cz_mats(nqc)
-    two_qubit_csr = csr_cz_mats(nqc)
-    two_qubit_csc = csc_cz_mats(nqc)
-    nqc.shutdown()
+    for num_qubits in range(2, 11):
+        memory_log.write(f"\nProfiling for {num_qubits} qubits:\n")
+        memory_log.flush()
+        nqc = create_circuit_object(num_qubits=num_qubits,
+                                    noise_model=noise_model,
+                                    num_cores=10,
+                                    num_trajectories=10,
+                                    threshold=1e-8,
+                                    jsonize=False,
+                                    verbose=False)
+        nqc.shutdown()
+        memory_log.write(f"Completed profiling for {num_qubits} qubits.\n")
+        memory_log.flush()
+    memory_log.close()
     print("Completed profiling memory usage.")
