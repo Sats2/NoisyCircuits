a72b88e8961f0a3c6faac9ac9e77581bd2cc19a3
Sats2 <sathyamhegde@gmail.com>
2026-06-17 15:29:24 +0200

    fixed minor issues with benchmark script

--- benchmarking/initialization_script.py
+++ benchmarking/initialization_script.py
@@ -1,55 +1,38 @@
-import pickle
-from NoisyCircuits import QuantumCircuit
-import os
 import numpy as np
 import json
 import sys
 
 np.random.seed(42)
 
-noise_model_file = os.path.join(os.path.expanduser("~"), "benchmarking_data/Noise_Model_for_Benchmark.pkl")
-noise_model = pickle.load(open(noise_model_file, "rb"))
 
-
-def build_random_circuit(circuit, depth):
+def build_random_circuit(num_qubits, depth):
     instruction_list = []
-    single_gates = {
-        "x" : lambda p, q: circuit.X(q),
-        "sx" : lambda p, q: circuit.SX(q),
-        "rz" : lambda p, q: circuit.RZ(p, q),
-        "rx" : lambda p, q: circuit.RX(p, q)
-    }
-    double_gates = {
-        "cz" : lambda p, q1, q2: circuit.CZ(q1, q2),
-        "rzz" : lambda p, q1, q2: circuit.RZZ(p, q1, q2)
-    }
+    single_gates = [
+        "x",
+        "rx",
+        "rz",
+        "sx"
+    ]
+    double_gates = [
+        "cz",
+        "rzz"
+    ]
     for _ in range(depth):
-        for i in range(circuit.num_qubits):
-            gate_name = np.random.choice(list(single_gates.keys()))
+        for q in range(num_qubits):
+            gate = np.random.choice(single_gates)
             param = np.random.uniform(-2*np.pi, 2*np.pi)
-            single_gates[gate_name](param, i)
-            instruction_list.append([gate_name, [i, i], param])
-        for q in range(circuit.num_qubits - 1):
-            gate_name = np.random.choice(list(double_gates.keys()))
+            instruction_list.append([gate, [q, q], param])
+        for q in range(num_qubits-1):
+            gate = np.random.choice(double_gates)
             param = np.random.uniform(-2*np.pi, 2*np.pi)
-            double_gates[gate_name](param, q, q + 1)
-            instruction_list.append([gate_name, [q, q+1], param])
+            instruction_list.append([gate, [q, q+1], param])
     return instruction_list
 
 if __name__ == "__main__":
     input_args = sys.argv[1:]
     qubits = int(input_args[0])
     depth = int(input_args[1])
-    circuit = QuantumCircuit(
-                num_qubits = qubits,
-                noise_model = noise_model,
-                backend_qpu_type = "heron",
-                sim_backend = "custom",
-                threshold = 1e-8,
-                verbose = False
-            )
-    circuit.refresh()
-    instruction_list = build_random_circuit(circuit, depth=depth)
+    instruction_list = build_random_circuit(qubits, depth=depth)
     save_loc = input_args[2]
     with open(save_loc, "w") as f:
         json.dump(instruction_list, f)
--- benchmarking/run_memory_benchmark.py
+++ benchmarking/run_memory_benchmark.py
@@ -5,13 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-trials = 35
+trials = 40
 max_qubits = 16
 trajectory_list = [10, 100, 500, 1000]
 file_location = os.path.join(os.path.expanduser("~"), "benchmarking_data/Memory/")
 
 data = {
-    "initialization" : {},
     "mcwf" : {},
     "density_matrix" : {}
 }
@@ -33,38 +32,22 @@ def benchmark_memory(qubits, trajectories, run_density_matrix, trial, file_loc, 
     match = re.search("Maximum resident set size \(kbytes\):\s+(\d+)", out)
     if match:
         mem = int(match.group(1))
-        return mem / 1024
+        return mem / 1000
     return None
 
-def benchmark_memory_initialization(qubits, depth, save_loc, trial, file_loc):
-    file = open(file_loc + "/Memory_Output_{}_Q_Initialization_Trial_{}.txt".format(qubits, trial), "w")
-    intro_line = "Trial No. {} for Circuit Initialization.\nMemory usage for {} Qubits, Depth {}.\n\n".format(trial, qubits, depth)
-    output = subprocess.run(["/usr/bin/time", "-v", "python3", "initialization_script.py", str(qubits), str(depth), str(save_loc)], capture_output=True, text=True)
-    out = output.stderr
-    file.write(intro_line)
-    file.write(out)
-    file.close()
-    match = re.search("Maximum resident set size \(kbytes\):\s+(\d+)", out)
-    if match:
-        mem = int(match.group(1))
-        return mem / 1024
-    return None
+def generate_random_circuit(qubits, file_loc, depth=100):
+    subprocess.run(["python3", "initialization_script.py", str(qubits), str(depth), str(file_loc)])
 
+consolidated_output = open(file_location + "Memory_Benchmark_Consolidated_Output.txt", "w")
 
-
-consolidated_output = open(file_location + "/Memory_Benchmark_Consolidated_Output.txt", "w")
 for qubits in range(2, max_qubits + 1):
     text = "Qubits: {}\n".format(qubits)
     qubit_data = {}
     file_loc_qubit = file_location + "{}_Qubits".format(qubits)
     os.mkdir(file_loc_qubit)
     
-    mem_usage_init_list = []
     for t in trajectory_list:
-        qubit_data[t] = {
-            "total_usage" : [],
-            "process_usage" : []
-        }
+        qubit_data[t] = []
     density_matrix_memory_usages = []
     density_matrix_memory_usages_full = []
     os.mkdir(file_loc_qubit + "/Circuit_Instructions")
@@ -74,43 +57,32 @@ for qubits in range(2, max_qubits + 1):
         file_loc_initialization = file_loc_qubit + "/Initialization"
         if not os.path.exists(file_loc_initialization):
             os.mkdir(file_loc_initialization)
-        mem_usage_init = benchmark_memory_initialization(qubits, 100, circuit_loc, trial, file_loc_initialization)
-        mem_usage_init_list.append(mem_usage_init)
+        generate_random_circuit(qubits, circuit_loc)
+        
         for trajectory_number in trajectory_list:
             file_loc_traj = file_loc_qubit + "/{}_Trajectories".format(trajectory_number)
             if not os.path.exists(file_loc_traj):
                 os.mkdir(file_loc_traj)
             memory_usage = benchmark_memory(qubits, trajectory_number, False, trial, file_loc_traj, circuit_loc)
-            qubit_data[trajectory_number]["total_usage"].append(memory_usage)
-            qubit_data[trajectory_number]["process_usage"].append(memory_usage - mem_usage_init)
+            qubit_data[trajectory_number].append(memory_usage)
         file_loc_dm = file_loc_qubit + "/Density_Matrix"
         if not os.path.exists(file_loc_dm):
             os.mkdir(file_loc_dm)
         memory_usage = benchmark_memory(qubits, trajectory_number, True, trial, file_loc_dm, circuit_loc)
-        density_matrix_memory_usages.append(memory_usage - mem_usage_init)
         density_matrix_memory_usages_full.append(memory_usage)
 
     data["mcwf"][qubits] = qubit_data
-    data["density_matrix"][qubits] = {
-        "total_usage" : density_matrix_memory_usages_full,
-        "process_usage" : density_matrix_memory_usages
-    }
-    data["initialization"][qubits] = mem_usage_init_list
+    data["density_matrix"][qubits] = density_matrix_memory_usages_full
+
     for trajectory_number in trajectory_list:
-        mean_total_usage = np.mean(qubit_data[trajectory_number]["total_usage"])
-        std_total_usage = np.std(qubit_data[trajectory_number]["total_usage"])
-        mean_process_usage = np.mean(qubit_data[trajectory_number]["process_usage"])
-        std_process_usage = np.std(qubit_data[trajectory_number]["process_usage"])
+        mean_total_usage = np.mean(qubit_data[trajectory_number])
+        std_total_usage = np.std(qubit_data[trajectory_number])
         text += "Trajectory Number: {}\n".format(trajectory_number)
-        text += "Mean Total Memory Usage: {:.2f} MB, Std Dev: {:.2f} MB\n".format(mean_total_usage, std_total_usage)
-        text += "Mean Process Memory Usage: {:.2f} MB, Std Dev: {:.2f} MB\n\n".format(mean_process_usage, std_process_usage)
-    mean_dm_usage = np.mean(density_matrix_memory_usages)
-    std_dm_usage = np.std(density_matrix_memory_usages)
+        text += "Mean Maximum Memory (RSS) Usage: {:.2f} MB, Std Dev: {:.2f} MB\n".format(mean_total_usage, std_total_usage)
     mean_dm_usage_full = np.mean(density_matrix_memory_usages_full)
     std_dm_usage_full = np.std(density_matrix_memory_usages_full)
     text += "Density Matrix Simulation:\n"
-    text += "Mean Total Memory Usage: {:.2f} MB, Std Dev: {:.2f} MB\n".format(mean_dm_usage_full, std_dm_usage_full)
-    text += "Mean Memory Usage: {:.2f} MB, Std Dev: {:.2f} MB\n".format(mean_dm_usage, std_dm_usage)
+    text += "Mean Maximum Memory (RSS) Usage: {:.2f} MB, Std Dev: {:.2f} MB\n".format(mean_dm_usage_full, std_dm_usage_full)
     text += "----------------------------------------\n\n"
     consolidated_output.write(text)
     consolidated_output.flush()
@@ -126,8 +98,8 @@ def plot_data(data, file_loc, traj_list=trajectory_list):
     mean_mem_usage_dm = []
     std_mem_usage_dm = []
     for qubits in qubit_list:
-        mean_mem_usage_dm.append(np.mean(data["density_matrix"][qubits]["process_usage"]))
-        std_mem_usage_dm.append(np.std(data["density_matrix"][qubits]["process_usage"]))
+        mean_mem_usage_dm.append(np.mean(data["density_matrix"][qubits]))
+        std_mem_usage_dm.append(np.std(data["density_matrix"][qubits]))
     plt.plot(qubit_list, mean_mem_usage_dm, label="Density Matrix")
     plt.fill_between(
         qubit_list,
@@ -139,8 +111,8 @@ def plot_data(data, file_loc, traj_list=trajectory_list):
         mean_mem_usage_mcwf = []
         std_mem_usage_mcwf = []
         for qubits in qubit_list:
-            mean_mem_usage_mcwf.append(np.mean(data["mcwf"][qubits][traj]["process_usage"]))
-            std_mem_usage_mcwf.append(np.std(data["mcwf"][qubits][traj]["process_usage"]))
+            mean_mem_usage_mcwf.append(np.mean(data["mcwf"][qubits][traj]))
+            std_mem_usage_mcwf.append(np.std(data["mcwf"][qubits][traj]))
         plt.plot(qubit_list, mean_mem_usage_mcwf, label="MCWF - {} Trajectories".format(traj))
         plt.fill_between(
             qubit_list,
@@ -149,8 +121,8 @@ def plot_data(data, file_loc, traj_list=trajectory_list):
             alpha=0.2
         )
     plt.xlabel("Number of Qubits")
-    plt.ylabel("Maximum Memory Usage (MB)")
-    plt.title("Memory Usage of Density Matrix vs MCWF Simulations (Process Memory Usage)")
+    plt.ylabel("Maximum Memory Usage (RSS) [MB]")
+    plt.title("Peak RSS Memory Usage of Density Matrix vs MCWF Simulations (Process Memory Usage)")
     plt.legend()
     plt.grid()
     plt.savefig(file_loc + "/Memory_Benchmark.svg")
